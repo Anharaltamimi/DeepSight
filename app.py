f0de38915d82abd7d9f8170f17e3a01978c7a3ce
shadenMhmd <niceandwanderful@gmail.com>
2025-11-20 20:15:58 +0300

    added port for deployment

--- app.py
+++ app.py
@@ -620,4 +620,6 @@ def update_doctor_profile():
 # Run
 # ===============================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
